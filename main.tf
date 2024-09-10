2aafb0599eb5e5eab6a978e61a3d1b77e0cbe699
rajeshb118 <163748589+rajeshb118@users.noreply.github.com>
2024-09-10 14:15:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_instance" "Mumbai" {
 
 # Create EC2 instances in us-west-1
 resource "aws_instance" "Ncalifornia" {
-  provider = aws.north-1
+  provider = aws.west-1
 
   ami           = "ami-025258b26b492aec6"  # Example AMI, replace with a valid one
   instance_type = "t3.micro"
